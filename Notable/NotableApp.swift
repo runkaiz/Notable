6fd812f7f9c64174ebdcf224c3b7b4c309c5474c
Runkai Zhang <rzhang04@proton.me>
2023-06-29 17:48:58 +0800

    Initial Commit

---
+++ Notable/NotableApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotableApp.swift
+//  Notable
+//
+//  Created by Runkai Zhang on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct NotableApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
